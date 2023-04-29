fe76bf71f1e6cfc8d8109390602cd4c14c337f02
leeheezin <gmlwls107@naver.com>
2023-04-30 01:39:58 +0900

    next 끗

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,60 @@
-import NavBar from "../components/NavBar";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useReducer, useState } from "react";
+import Seo from "../components/Seo";
 
-export default function Home(){
+export default function Home({results}){
+    const router = useRouter()
+    const onClick = (id, title) => {
+        router.push(`/movies/${title}/${id}`)
+    }
+    
     return (
-        <div>
-            <NavBar/>
-            <h1>Home</h1>
+        <div className="container">
+            <Seo title="Home" />
+            {results?.map((movie) => (
+                <Link href={`/movies/${movie.original_title}/${movie.id}`} key={movie.id}>
+                    <div onClick={()=> onClick(movie.id, )}className="movie" key={movie.id}>
+                        <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+                        <h4>{movie.original_title}</h4>
+                    </div>
+                </Link>
+            ) )}
+            <style jsx>{`
+                .container {
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                padding: 20px;
+                gap: 20px;
+                }
+                .movie {
+                    cursor: pointer;
+                }
+                .movie img {
+                max-width: 100%;
+                border-radius: 12px;
+                transition: transform 0.2s ease-in-out;
+                box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+                }
+                .movie:hover img {
+                transform: scale(1.05) translateY(-10px);
+                }
+                .movie h4 {
+                font-size: 18px;
+                text-align: center;
+                }
+            `}</style>
         </div>
     )
+}
+
+export async function getServerSideProps() {
+    const { results } = await (await fetch(
+        `http://localhost:3000/api/movies`
+        )).json()
+    return {
+        props: {
+            results,
+        }
+    }
 }
